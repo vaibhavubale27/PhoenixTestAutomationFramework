6013553baa4b8c2c10841cc3d09c758280e4ccd5
Vaibhav Ubale <vaibhavubale797@gmail.com>
2026-04-08 05:53:26 +0530

    Practiced mapping of CSV file to pojo

---
+++ src/main/java/com/demo/csv/ReadCSVFile_MapToPojo.java
@@ -0,0 +1,42 @@
+package com.demo.csv;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvException;
+
+public class ReadCSVFile_MapToPojo {
+
+	public static void main(String[] args) throws IOException, CsvException {
+		
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
+		
+		InputStreamReader isr=new InputStreamReader(inputStream);
+		
+		CSVReader csvReader=new CSVReader(isr); //CSV Reader constructor require Reader
+		
+		//Map the code 
+		
+		CsvToBean<UserPOJO> csvTobean=new CsvToBeanBuilder(csvReader)
+				.withType(UserPOJO.class)
+				.withIgnoreEmptyLine(true)
+				.build();
+		
+		List<UserPOJO> userList = csvTobean.parse();
+		
+		System.out.print(userList);
+		
+		
+		
+
+
+	}
+
+}
